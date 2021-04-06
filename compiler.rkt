128f5834c7571fe01c9d938f130444b6635d6612
iambrj <joshibharathiramana@gmail.com>
2021-04-06 19:22:18 +0530

    Implement remove-complex-opera* pass

--- compiler.rkt
+++ compiler.rkt
@@ -39,8 +39,8 @@
   (match e
     [(Int n) (Int n)]
     [(Prim 'read '()) (Prim 'read '())]
-    [(Prim '- (list e1)) (pe-neg (pe-exp e1))]
-    [(Prim '+ (list e1 e2)) (pe-add (pe-exp e1) (pe-exp e2))]))
+    [(Prim '- `(,e1)) (pe-neg (pe-exp e1))]
+    [(Prim '+ `(,e1 ,e2)) (pe-add (pe-exp e1) (pe-exp e2))]))
 
 (define (pe-Rint p)
   (match p
@@ -70,9 +70,80 @@
   (match p
     [(Program info e) (Program info ((uniquify-exp '()) e))]))
 
+
+(define (rco-atm e)
+  (match e
+    [(Int i)
+     (values (Int i)
+             '())]
+    [(Var v)
+     (values (Var v)
+             '())]
+    [(Prim 'read '())
+     (let ([tmp (gensym 'tmp)])
+      (values tmp
+             `((,tmp . ,(Prim 'read '())))))]
+    [(Prim '- `(,e1))
+     (let ([tmp (gensym 'tmp)])
+       (values tmp
+               `((,tmp . ,(Prim '- `(,e1))))))]
+    [(Prim '+ `(,e1 ,e2))
+     (define-values (tmp1 tmp->exp1) (rco-atm e1))
+     (define-values (tmp2 tmp->exp2) (rco-atm e2))
+     (cond
+       [(and (dict-empty? tmp->exp1) (dict-empty? tmp->exp2))
+        (Prim '+ `(,e1 ,e2))]
+       [(and (not (dict-empty? tmp->exp1)) (dict-empty? tmp->exp2))
+        (values (Prim '+ `(,(Var tmp1) ,e2))
+                tmp->exp1)]
+       [(and (dict-empty? tmp->exp1) (not (dict-empty? tmp->exp2)))
+        (values (Prim '+ `(,e1 ,(Var tmp2)))
+                tmp->exp2)]
+       [(and (not (dict-empty? tmp->exp1)) (not (dict-empty? tmp->exp2)))
+        (values (Prim '+ `(,(Var tmp1) ,(Var tmp2)))
+                `((,tmp1 . ,(dict-ref tmp->exp1 tmp1))
+                  (,tmp2 . ,(dict-ref tmp->exp2 tmp2))))])]
+    [(Let x v b)
+     (let ([tmp (gensym 'tmp)])
+       (values tmp
+               `((,tmp . ,(Let x (rco-exp v) (rco-exp b))))))]))
+
+(define (rco-exp e)
+  (match e
+    [(Int i) (Int i)]
+    [(Var v) (Var v)]
+    [(Prim 'read '())
+     (Prim 'read '())]
+    [(Prim '- `(,e1))
+     (define-values (tmp tmp->expr) (rco-atm e1))
+     (if (dict-empty? tmp->expr)
+       (Prim '- `(,e1))
+       (Let tmp (dict-ref tmp->expr tmp) (Prim '- `(,(Var tmp)))))]
+    [(Prim '+ `(,e1 ,e2))
+     (define-values (tmp1 tmp->exp1) (rco-atm e1))
+     (define-values (tmp2 tmp->exp2) (rco-atm e2))
+     (cond
+       [(and (dict-empty? tmp->exp1) (dict-empty? tmp->exp2))
+        (Prim '+ `(,e1 ,e2))]
+       [(and (not (dict-empty? tmp->exp1)) (dict-empty? tmp->exp2))
+        (Let tmp1 (dict-ref tmp->exp1 tmp1) (Prim '+ `(,(Var tmp1) ,e2)))]
+       [(and (dict-empty? tmp->exp1) (not (dict-empty? tmp->exp2)))
+        (Let tmp2 (dict-ref tmp->exp2 tmp2) (Prim '+ `(,e1 ,(Var tmp2))))]
+       [(and (not (dict-empty? tmp->exp1)) (not (dict-empty? tmp->exp2)))
+        (Let tmp1
+             (dict-ref tmp->exp1 tmp1)
+             (Let tmp2
+                  (dict-ref tmp->exp2 tmp2)
+                  (Prim '+ `(,(Var tmp1) ,(Var tmp2)))))])]
+    [(Let x v b)
+     (Let x (rco-exp v) (rco-exp b))]))
+
 ;; remove-complex-opera* : R1 -> R1
+;; arguments of operations are atomic
 (define (remove-complex-opera* p)
-  (error "TODO: code goes here (remove-complex-opera*)"))
+  (match p
+    [(Program info e)
+     (rco-exp e)]))
 
 ;; explicate-control : R1 -> C0
 (define (explicate-control p)
