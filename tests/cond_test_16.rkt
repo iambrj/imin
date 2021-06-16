a02104f795e11baba8b2df173c4061e69640891f
iambrj <joshibharathiramana@gmail.com>
2021-06-16 08:29:37 +0530

    Add/update tests

--- tests/cond_test_16.rkt
+++ tests/cond_test_16.rkt
@@ -1,36 +1,3 @@
 (if (and (eq? (read) 0) (eq? (read) 1))
   31
   40)
-#|
-(define p1 (Program
-             '()
-             (If
-               (If
-                 (Let 'tmp27281 (Prim 'read '()) (Prim 'eq? (list (Var 'tmp27281) (Int 0))))
-                 (Let 'tmp27282 (Prim 'read '()) (Prim 'eq? (list (Var 'tmp27282) (Int 1))))
-                 (Bool #f))
-               (Int 0)
-               (Int 42))))
-
-=>
-
-block25000:
-  Return 0
-block25001:
-  Return 42
-block25002:
-  IfStmt (eq? var27282 1)
-  Goto block25000
-  Goto block25001
-block25003:
-  tmp27282 = (read)
-  Goto block25002
-block25004:
-  IfStmt (eq? tmp27281 0)
-  Goto block25003
-  Goto block25001
-start:
-  tmp27281 = (read)
-  Goto block25004
-
-|#
---
+++ tests/cond_test_19.rkt
@@ -0,0 +1,5 @@
+(let ([x (read)])
+  (let ([y (read)])
+    (if (if (< x 1) (eq? x 0) (eq? x 2))
+      (+ y 2)
+      (+ y 10))))
