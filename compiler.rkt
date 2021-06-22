f5b34db6cb5941043bf5ed28ff63926ea0307f44
iambrj <joshibharathiramana@gmail.com>
2021-06-22 05:54:53 +0530

    Rfun : implement limit functions pass

--- compiler.rkt
+++ compiler.rkt
@@ -70,12 +70,12 @@ r15 -> shadow stack top
      (Prim op (map shrink-exp es))]
     [(Prim 'vector-ref `(,e ,(Int i)))
      (Prim 'vector-ref `(,(shrink-exp e) ,(Int i)))]
+    [(Prim 'vector-set! `(,e1 ,(Int i) ,e2))
+     (Prim 'vector-set! `(,(shrink-exp e1) ,(Int i) ,(shrink-exp e2)))]
     [(Apply fun arg*)
      (Apply (shrink-exp fun) (map shrink-exp arg*))]
     [(Def name param* rty info e)
      (Def name param* rty info (shrink-exp e))]
-    [(Prim 'vector-set! `(,e1 ,(Int i) ,e2))
-     (Prim 'vector-set! `(,(shrink-exp e1) ,(Int i) ,(shrink-exp e2)))]
     ; (and e1 e1) == (if e1 e2 #f)
     [(Prim 'and `(,e1 ,e2))
      (let ([e1 (shrink-exp e1)]
@@ -139,6 +139,97 @@ r15 -> shadow stack top
        (ProgramDefs info (map (lambda (d)
                                 (struct-copy Def d
                                              [body ((rf-exp funs) (Def-body d))]))
+                              defs)))]))
+
+(define ((update-body vec vectorized) body)
+  (if (not (null? vectorized))
+    (match body
+      [(Void)         (Void)]
+      [(Int i)        (Int i)]
+      [(Bool b)       (Bool b)]
+      [(Var v)        (if (member v vectorized)
+                        (Prim 'vector-ref `(,(Var vec) ,(Int (index-of vectorized v))))
+                        (Var v))]
+      [(HasType e t)  (HasType ((update-body vec vectorized) e) t)]
+      [(If c t e)     (If ((update-body vec vectorized) c)
+                          ((update-body vec vectorized) t)
+                          ((update-body vec vectorized) e))]
+      [(Let v e b)
+       ; XXX : Trade off between this trick and deleteing elements?
+       (let ([vectorized1 (if (member v vectorized)
+                            (list-update vectorized
+                                         (index-of vectorized v)
+                                         (gensym))
+                            vectorized)])
+         (Let v ((update-body vec vectorized) e)
+              ((update-body vec vectorized1) b)))]
+      [(Prim op es) #:when (member op '(read - + not < vector vector-length))
+       (Prim op (map (update-body vec vectorized) es))]
+      [(Prim 'vector-ref `(,e ,(Int i)))
+       (Prim 'vector-ref `(,((update-body vec vectorized) e) ,(Int i)))]
+      [(Prim 'vector-set! `(,e1 ,(Int i) ,e2))
+       (Prim 'vector-set! `(,((update-body vec vectorized) e1)
+                             ,(Int i)
+                             ,((update-body vec vectorized) e2)))]
+      ; Invariant : only top level function definitions are allowed, no clause
+      ; needed for Def
+      [(Apply fun arg*)
+       (Apply ((update-body vec vectorized) fun) (map (update-body vec vectorized) arg*))])
+    body))
+
+(define (limit-def d)
+  (match d
+    [(Def name param* rty info body)
+     (let* ([vec (gensym 'arg6-)]
+            [types (map caddr param*)]
+            [vectorized (if (> (length param*) 6)
+                          (map car (drop param* 5))
+                          '())]
+            [param* (if (> (length param*) 6)
+                     (append (take param* 5)
+                             `((,vec : ,(cons 'Vector (drop types 5)))))
+                     param*)])
+       (Def name param* rty info ((update-body vec vectorized) body)))]))
+
+(define (limit-calls e)
+  (match e
+      [(Void)         (Void)]
+      [(Int i)        (Int i)]
+      [(Bool b)       (Bool b)]
+      [(Var v)        (Var v)]
+      [(FunRef f)     (FunRef f)]
+      [(HasType e t)  (HasType (limit-calls e) t)]
+      [(If c t e)     (If (limit-calls c)
+                          (limit-calls t)
+                          (limit-calls e))]
+      [(Let v e b)
+       ; XXX : Trade off between this trick and deleteing elements?
+       (Let v (limit-calls e) (limit-calls b))]
+      [(Prim op es) #:when (member op '(read - + not < vector vector-length))
+       (Prim op (map limit-calls es))]
+      [(Prim 'vector-ref `(,e ,(Int i)))
+       (Prim 'vector-ref `(,(limit-calls e) ,(Int i)))]
+      [(Prim 'vector-set! `(,e1 ,(Int i) ,e2))
+       (Prim 'vector-set! `(,(limit-calls e1)
+                             ,(Int i)
+                             ,(limit-calls e2)))]
+      ; Invariant : only top level function definitions are allowed, no clause
+      ; needed for Def
+      [(Apply fun arg*)
+       (Apply (limit-calls fun)
+              (let ([arg* (if (> (length arg*) 6)
+                            (append (take arg* 5)
+                             `(,(Prim 'vector (drop arg* 5))))
+                            arg*)])
+                (map limit-calls arg*)))]))
+
+(define (limit-functions p)
+  (match p
+    [(ProgramDefs info defs)
+     (let ([defs (map limit-def defs)])
+       (ProgramDefs info (map (lambda (d)
+                                (struct-copy Def d
+                                             [body (limit-calls (Def-body d))]))
                               defs)))]))
 
 (define (ccollect bytes)
--- run-tests.rkt
+++ run-tests.rkt
@@ -2,15 +2,9 @@
 #lang racket
 
 (require "utilities.rkt"
-         "interp-Rvec-prime.rkt"
-         "interp-Rvec.rkt"
-         "type-check-Rvec.rkt"
-         "interp-Cvec.rkt"
-         "type-check-Cvec.rkt"
-         "interp-Rif.rkt"
-         "type-check-Rif.rkt"
-         "interp-Cif.rkt"
-         "type-check-Cif.rkt"
+         "interp-Rfun.rkt"
+         "interp-Rfun-prime.rkt"
+         "type-check-Rfun.rkt"
          "interp.rkt"
          "compiler.rkt")
 ;; (debug-level 1)
@@ -20,6 +14,21 @@
 ;; Note that your compiler file (the file that defines the passes)
 ;; should be named "compiler.rkt"
 (define passes
+  `(("shrink" ,shrink ,interp-Rfun ,type-check-Rfun)
+    ("reveal functions" ,reveal-functions ,interp-Rfun-prime ,type-check-Rfun)
+    ("limit functions" ,limit-functions ,interp-Rfun-prime ,type-check-Rfun)
+    #;("expose allocation" ,expose-allocation ,interp-Rfun-prime ,type-check-Rfun)
+    #;("remove complex opera*" ,remove-complex-opera* ,interp-Rfun-prime ,type-check-Rfun)
+    #;("explicate control" ,explicate-control ,interp-Cvec ,type-check-Cvec)
+    #;("select instructions" ,select-instructions ,interp-pseudo-x86-2)
+    #;("build cfg" ,build-cfg ,interp-pseudo-x86-2)
+    #;("uncover live" ,uncover-live ,interp-pseudo-x86-2)
+    #;("build interference" ,build-interference ,interp-pseudo-x86-2)
+    #;("allocate registers" ,allocate-registers ,interp-pseudo-x86-2)
+    #;("patch instructions" ,patch-instructions ,interp-pseudo-x86-2)
+    #;("print x86" ,print-x86 #f)))
+
+#;(define passes
   `(("shrink" ,shrink ,interp-Rvec ,type-check-Rvec)
     ("expose allocation" ,expose-allocation ,interp-Rvec-prime ,type-check-Rvec)
     ("remove complex opera*" ,remove-complex-opera* ,interp-Rvec-prime ,type-check-Rvec)
@@ -31,19 +40,6 @@
     ("allocate registers" ,allocate-registers ,interp-pseudo-x86-2)
     ("patch instructions" ,patch-instructions ,interp-pseudo-x86-2)
     ("print x86" ,print-x86 #f)))
-#;(define passes
-  `(("shrink" ,shrink ,interp-Rif ,type-check-Rif)
-    ("uniquify" ,uniquify ,interp-Rif ,type-check-Rif)
-    ("remove complex opera*" ,remove-complex-opera* ,interp-Rif ,type-check-Rif)
-    ("explicate control" ,explicate-control ,interp-Cif ,type-check-Cif)
-    ("select instructions" ,select-instructions ,interp-pseudo-x86-1)
-    ("build cfg" ,build-cfg ,interp-pseudo-x86-1)
-    ("uncover live" ,uncover-live ,interp-pseudo-x86-1)
-    ("build interference" ,build-interference ,interp-pseudo-x86-1)
-    ("allocate registers" ,allocate-registers ,interp-pseudo-x86-1)
-    ("patch instructions" ,patch-instructions ,interp-pseudo-x86-1)
-    ("print x86" ,print-x86 #f)))
-
 ;; all the files in the tests/ directory with extension ".rkt".
 (define all-tests
   (map (lambda (p) (car (string-split (path->string p) ".")))
@@ -61,9 +57,10 @@
 
 (debug-level 1)
 ;(interp-tests "cond" type-check-Rif passes interp-Rvec "cond_test" (tests-for "cond"))
-(interp-tests "vec" type-check-Rvec passes interp-Rvec "vectors_test" (tests-for "vectors"))
+(interp-tests "functions" type-check-Rfun passes interp-Rfun "functions_test"
+              (tests-for "functions"))
 
 ;; Uncomment the following when all the passes are complete to
 ;; test the final x86 code.
 ;(compiler-tests "cond" type-check-Rif passes "cond_test" (tests-for "cond"))
-(compiler-tests "vectors" type-check-Rvec passes "vectors_test" (tests-for "vectors"))
+;(compiler-tests "vectors" type-check-Rvec passes "vectors_test" (tests-for "vectors"))
