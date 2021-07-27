11ac1b912dafd2fa997905c6ad46d7463a41000c
iambrj <joshibharathiramana@gmail.com>
2021-07-27 09:43:35 +0530

    Rfun: refactor register allocation

--- compiler.rkt
+++ compiler.rkt
@@ -994,31 +994,67 @@ r15 -> shadow stack top
     [(ProgramDefs info def*)
      (ProgramDefs info (map bi-def def*))]))
 
-(define (min-free-list l)
-  (match l
-    [`(,a) (add1 a)]
-    [`(,x . (,y . ,d))
-      (if (= (add1 x) y)
-        (min-free-list `(,y . ,d))
-        (add1 x))]))
+(define (allocate-registers d)
+  (match d
+    [(ProgramDefs info def*)
+     (ProgramDefs info (map ar-def def*))]))
 
-; NOTE : Can be optimized to O(n) using some obscure mutation tricks
-(define (min-free s)
-  (let* ([l (set->list s)]
-         [l (sort l <)])
-    (if (or (equal? '() l) (> (car l) 0))
-      0
-      (min-free-list l))))
+(define (ar-def d)
+  (match d
+    [(Def name param* rty info label-block*)
+     (let*-values ([(ig) (dict-ref info 'conflicts)]
+                   [(vertex->color) (color-graph ig)]
+                   [(var->mem stack-space shadow-stack-space)
+                    (var->mem vertex->color)]
+                   [(label-block*) (substitute var->mem label-block*)]
+                   [(used-callee)
+                    (filter (lambda (r)
+                              (member r callee-saved))
+                            (remove-duplicates (dict-values var->mem)))])
+       (Def name param* rty (dict-set* info
+                                       'stack-space stack-space
+                                       'shadow-stack-space shadow-stack-space
+                                       'var->mem var->mem
+                                       'used-callee used-callee)
+            label-block*))]))
 
-(define (update-neighbors! vertex->satur color neighbors)
-  (match neighbors
-    [`() vertex->satur]
-    [`(,n . ,d)
-      (let* ([s (dict-ref vertex->satur n)]
-             [_ (dict-set! vertex->satur n (set-union s (set color)))])
-        (update-neighbors! vertex->satur color d))]))
+(define (color-graph ig)
+  (let* ([vars (filter Var? (get-vertices ig))]
+         [regs (filter Reg? (get-vertices ig))]
+         [vertex->color (init-vertex->color regs)]
+         [vertex->sat (init-vertex->sat vars ig vertex->color)]
+         [q (init-pq vertex->sat)]
+         [vertex->node (map (lambda (v) (cons v (pqueue-push! q v))) vars)])
+    (color-graph-iter ig q vars vertex->sat vertex->node
+                      vertex->color)))
 
-(define (color-graph g q w vertex->satur vertex->node vertex->color)
+(define (init-vertex->color regs)
+  (foldr (lambda (reg a)
+           (if (member reg usable-registers)
+             (dict-set a reg (index-of
+                               usable-registers
+                               reg))
+             a))
+         '()
+         regs))
+
+(define (init-vertex->sat vars ig vertex->color)
+  (make-hash (foldr (lambda (v a)
+                      (let* ([n (get-neighbors ig v)]
+                             [s (foldr (lambda (n a)
+                                         (if (dict-has-key? vertex->color n)
+                                           (set-union a (set (dict-ref vertex->color n)))
+                                           a))
+                                       (set)
+                                       n)])
+                        `((,v . ,s) . ,a))) '() vars)))
+
+(define (init-pq vertex->sat)
+  (make-pqueue (lambda (x1 x2)
+                 (> (set-count (dict-ref vertex->sat x1))
+                    (set-count (dict-ref vertex->sat x2))))))
+
+(define (color-graph-iter g q w vertex->satur vertex->node vertex->color)
   (match w
     ['() vertex->color]
     [else (let* ([v (pqueue-pop! q)]
@@ -1031,9 +1067,17 @@ r15 -> shadow stack top
                            '()
                            neighbors)]
                  [vertex->satur (update-neighbors! vertex->satur color neighbors)])
-            (color-graph g q w vertex->satur vertex->node (dict-set
-                                                            vertex->color v
-                                                            color)))]))
+            (color-graph-iter g q w vertex->satur vertex->node (dict-set
+                                                                 vertex->color v
+                                                                 color)))]))
+
+(define (update-neighbors! vertex->satur color neighbors)
+  (match neighbors
+    [`() vertex->satur]
+    [`(,n . ,d)
+      (let* ([s (dict-ref vertex->satur n)]
+             [_ (dict-set! vertex->satur n (set-union s (set color)))])
+        (update-neighbors! vertex->satur color d))]))
 
 (define (var->mem var->color)
   (let* ([color->reg (map (lambda (r)
@@ -1041,7 +1085,7 @@ r15 -> shadow stack top
                           usable-registers)]
          [maxcolor (apply max (dict-keys color->reg))]
          [spills (spill maxcolor var->color)]
-         [spilled->stk (get-stk-locations spills)])
+         [spilled->stk (spill->stk spills)])
     (values (dict-map var->color (lambda (v c)
                                    `(,v . ,(if (member v spills)
                                              (dict-ref spilled->stk v)
@@ -1054,7 +1098,7 @@ r15 -> shadow stack top
             (> (dict-ref var->color v) maxcolor))
           (dict-keys var->color)))
 
-(define (get-stk-locations spills)
+(define (spill->stk spills)
   (let ([vec-spills (filter Vector? spills)]
         [nonvec-spills (filter Var? spills)])
     (foldr (lambda (v i a)
@@ -1070,80 +1114,48 @@ r15 -> shadow stack top
 (define (stack-size pred spills)
   (length (filter pred spills)))
 
-(define ((ar-arg var->mem) arg)
-  (match arg
-    [(Var v) (dict-ref var->mem (Var v))]
-    [_ arg]))
+; NOTE : Can be optimized to O(n) using some obscure mutation tricks
+(define (min-free s)
+  (let* ([l (set->list s)]
+         [l (sort l <)])
+    (if (or (equal? '() l) (> (car l) 0))
+      0
+      (min-free-list l))))
 
-(define ((ar-instr var->mem) instr)
-  (match instr
-    [(Instr i args) (Instr i (map (ar-arg var->mem) args))]
-    [(TailJmp v c) (TailJmp ((ar-arg var->mem) v) c)]
-    [_ instr]))
-
-(define ((ar-block var->mem) blk)
-  (match blk
-    [(Block info instrs)
-     (Block info (map (ar-instr var->mem) instrs))]))
-
-(define (ar-def d)
-  (match d
-    [(Def name param* rty info label-block*)
-     (let* ([g (dict-ref info 'conflicts)]
-            [v (filter Var? (get-vertices g))]
-            [r (filter Reg? (get-vertices g))]
-            [vertex->color (foldr (lambda (r a)
-                                    (if (member r usable-registers)
-                                      (dict-set a r (index-of
-                                                      usable-registers
-                                                      r))
-                                      a))
-                                  '()
-                                  r)]
-            [vertex->sat
-              (make-hash (foldr (lambda (v a)
-                                  (let* ([n (get-neighbors g v)]
-                                         [s (foldr (lambda (n a)
-                                                     (if (dict-has-key? vertex->color n)
-                                                       (set-union a (set (dict-ref vertex->color n)))
-                                                       a))
-                                                   (set)
-                                                   n)])
-                                    `((,v . ,s) . ,a))) '() v))]
-            [q
-              (make-pqueue (lambda (x1 x2)
-                             (> (set-count (dict-ref vertex->sat x1))
-                                (set-count (dict-ref vertex->sat x2)))))]
-            [vertex->node
-              (map (lambda (v) (cons v (pqueue-push! q v))) v)]
-            [vertex->color
-              (color-graph g q v vertex->sat vertex->node
-                           vertex->color)])
-       (let*-values ([(var->mem stack-space shadow-stack-space)
-                      (var->mem vertex->color)]
-                     [(used-callee)
-                      (filter (lambda (r)
-                                (member r callee-saved))
-                              (remove-duplicates (dict-values var->mem)))])
-         (Def name param* rty (dict-set* info
-                                         'stack-space stack-space
-                                         'shadow-stack-space shadow-stack-space
-                                         'var->mem var->mem
-                                         'used-callee used-callee)
-              (map (lambda (label-block)
-                     (cons (car label-block)
-                           ((ar-block var->mem) (cdr label-block))))
-                   label-block*))))]))
-
-(define (allocate-registers d)
-  (match d
-    [(ProgramDefs info def*)
-     (ProgramDefs info (map ar-def def*))]))
+(define (min-free-list l)
+  (match l
+    [`(,a) (add1 a)]
+    [`(,x . (,y . ,d))
+      (if (= (add1 x) y)
+        (min-free-list `(,y . ,d))
+        (add1 x))]))
 
 (define (trivial-mov instr)
   (match instr
     [(Instr 'movq `(,a ,a)) #t]
     [_ #f]))
+
+(define (substitute var->mem label-block*)
+  (map (lambda (label-block)
+         (cons (car label-block)
+               ((substitute-block var->mem) (cdr label-block))))
+       label-block*))
+
+(define ((substitute-block var->mem) blk)
+  (match blk
+    [(Block info instrs)
+     (Block info (map (substitute-instr var->mem) instrs))]))
+
+(define ((substitute-instr var->mem) instr)
+  (match instr
+    [(Instr i args) (Instr i (map (substitute-arg var->mem) args))]
+    [(TailJmp v c) (TailJmp ((substitute-arg var->mem) v) c)]
+    [_ instr]))
+
+(define ((substitute-arg var->mem) arg)
+  (match arg
+    [(Var v) (dict-ref var->mem (Var v))]
+    [_ arg]))
 
 (define (pi-instr instr)
   (match instr
