29e41f0913452905e919082ff3818550af3370ec
iambrj <joshibharathiramana@gmail.com>
2021-07-28 07:08:13 +0530

    Rfun: refactor print-x86 [buggy]

--- compiler.rkt
+++ compiler.rkt
@@ -1203,18 +1203,40 @@ r15 -> shadow stack top
     [(ProgramDefs info def*)
      (ProgramDefs info (map pi-def def*))]))
 
-(define (print-x86-arg arg)
-  (match arg
-    [(Global x) (format "~a(%rip)" x)]
-    [(Imm i) (format "$~a" i)]
-    [(Reg r) (format "%~a" r)]
-    [(Deref r i) (format "~a(%~a)" i r)]
-    [(FunRef l) (format "~a(%rip)" l)]))
+(define (print-x86 p)
+  (match p
+    [(ProgramDefs info def*)
+     (let ([l (map print-x86-def def*)])
+       (apply string-append l))]))
 
-(define (print-x86-args args)
-  (match args
-    [`(,a) (print-x86-arg a)]
-    [`(,a1 ,a2) (format "~a, ~a" (print-x86-arg a1) (print-x86-arg a2))]))
+;; print-x86 : x86 -> string
+(define (print-x86-def p)
+  (match p
+    [(Def name param* rty info label-block*)
+     (let* ([stack-space (dict-ref info 'stack-space)]
+            [shadow-stack-space (dict-ref info 'shadow-stack-space)]
+            [used-callee (dict-ref info 'used-callee)]
+            [prelude (print-x86-prelude name stack-space shadow-stack-space used-callee)]
+            [conclusion (print-conclusion-block name stack-space shadow-stack-space used-callee)]
+            [label-block* (let ([l (for/list ([label-block label-block*])
+                                     (string-append (format "~a:\n" (car label-block))
+                                                    (print-x86-block (cdr label-block)
+                                                                     stack-space
+                                                                     shadow-stack-space
+                                                                     used-callee)))])
+                            (apply string-append l))])
+       (string-append prelude label-block* conclusion))]))
+
+(define (print-x86-block blk stack-space shadow-stack-space used-callee)
+  (match blk
+    [(Block info instr*)
+     (let ([l (for/list ([instr instr*])
+                (format "\t~a\n" (print-x86-instr stack-space
+                                                  shadow-stack-space
+                                                  used-callee
+                                                  instr)))])
+       (apply string-append l))]
+    [_ (error "print-x86-block unhandled case " blk)]))
 
 (define (print-x86-instr stack-space shadow-stack-space used-callee instr)
   (match instr
@@ -1231,94 +1253,85 @@ r15 -> shadow stack top
      (string-append (print-restore stack-space shadow-stack-space used-callee)
                     (format "jmp *~a" (print-x86-arg arg)))]))
 
-(define (print-x86-block blk stack-space shadow-stack-space used-callee)
-  (match blk
-    [(Block info instrs)
-     (foldr (lambda (instr acc)
-              (string-append acc
-                             (format "\t~a\n" (print-x86-instr stack-space
-                                                               shadow-stack-space
-                                                               used-callee
-                                                               instr))))
-            ""
-            instrs)]
-    [_ (error "print-x86-block unhandled case " blk)]))
+(define (print-x86-args args)
+  (match args
+    [`(,a) (print-x86-arg a)]
+    [`(,a1 ,a2) (format "~a, ~a" (print-x86-arg a1) (print-x86-arg a2))]))
+
+(define (print-x86-arg arg)
+  (match arg
+    [(Global x) (format "~a(%rip)" x)]
+    [(Imm i) (format "$~a" i)]
+    [(Reg r) (format "%~a" r)]
+    [(Deref r i) (format "~a(%~a)" i r)]
+    [(FunRef l) (format "~a(%rip)" l)]))
 
 (define (print-x86-prelude name stack-space shadow-stack-space used-callee)
-  (string-append (format "\t.globl ~a\n" name)
-                 "\t.align 16\n"
+  (string-append (print-directives name)
                  (format "~a:\n" name)
                  "\tpushq %rbp\n"
                  "\tmovq %rsp, %rbp\n"
-                 (foldr (lambda (r a)
-                          (format "\tpushq %~a\n" (Reg-name r)))
-                        ""
-                        used-callee)
-                 (format "\tsubq $~a, %rsp\n" (print-alignment stack-space
-                                                               used-callee))
+                 (print-pushes used-callee)
+                 (print-alignment-setup stack-space used-callee)
                  (if (eqv? name 'main)
-                   (string-append
-                     "\tmovq $16384, %rdi\n"
-                     "\tmovq $16384, %rsi\n"
-                     "\tcallq initialize\n"
-                     "\tmovq rootstack_begin(%rip), %r15\n")
+                   print-gc-init
                    "")
-                 "\tmovq $0, %r15\n"
-                 (format "\taddq $~a, %r15\n" (number->string shadow-stack-space))
-                 (format "\tjmp ~astart\n" (symbol->string name))))
+                 (print-gc-setup shadow-stack-space)
+                 (format "\tjmp ~astart\n" name)))
 
-(define (print-alignment stack-space used-callee)
-  (if (zero? (modulo (+ stack-space
-                        (* 8 (length used-callee)))
-                     16))
-    stack-space
-    (+ 8 stack-space)))
+(define (print-alignment-setup stack-space used-callee)
+  (format "\tsubq $~a, %rsp\n" (if (zero? (modulo (+ stack-space
+                                                     (* 8 (length used-callee)))
+                                                  16))
+                                 stack-space
+                                 (+ 8 stack-space))))
 
-(define (print-restore stack-space shadow-stack-space used-callee)
-  (string-append "\tsubq $" (number->string shadow-stack-space) ", %r15\n"
-                 "\taddq $" (number->string (if (zero? (modulo (+ stack-space
-                                                                  (* 8 (length used-callee)))
-                                                               16))
-                                              stack-space
-                                              (+ 8 stack-space))) ", %rsp\n"
-                 (apply string-append
-                        (map (lambda (r)
-                               (string-append "\tpopq %"
-                                              (symbol->string (Reg-name r))
-                                              "\n"))
-                             used-callee))
-                 "\tpopq %rbp\n"))
+(define (print-directives name)
+  (string-append (format "\t.globl ~a\n" name)
+                 "\t.align 16\n"))
+
+(define print-gc-init
+  (string-append
+    "\tmovq $16384, %rdi\n"
+    "\tmovq $16384, %rsi\n"
+    "\tcallq initialize\n"
+    "\tmovq rootstack_begin(%rip), %r15\n"))
+
+(define (print-gc-setup shadow-stack-space)
+  (string-append "\tmovq $0, %r15\n"
+                 (if (> shadow-stack-space 0)
+                   (format "\taddq $~a, %r15\n" shadow-stack-space)
+                   "")))
+
+(define (print-pushes used-callee)
+  (let ([l (for/list ([r used-callee])
+             (format "\tpushq %~a\n" (Reg-name r)))])
+    (apply string-append l)))
 
 (define (print-conclusion-block name stack-space shadow-stack-space used-callee)
-  (string-append (format "~aconclusion:\n" (symbol->string name))
+  (string-append (format "~aconclusion:\n" name)
                  (print-restore stack-space shadow-stack-space used-callee)
                  "\tretq\n"))
 
-;; print-x86 : x86 -> string
-(define (print-x86-def p)
-  (match p
-    [(Def name param* rty info label-block*)
-     (let* ([stack-space (dict-ref info 'stack-space)]
-            [shadow-stack-space (dict-ref info 'shadow-stack-space)]
-            [used-callee (dict-ref info 'used-callee)]
-            [prelude (print-x86-prelude name stack-space shadow-stack-space used-callee)]
-            [conclusion (print-conclusion-block name stack-space shadow-stack-space used-callee)]
-            [label-block* (foldl (lambda (label-block acc)
-                                   (string-append (symbol->string (car label-block)) ":\n"
-                                                  (print-x86-block (cdr label-block)
-                                                                   stack-space
-                                                                   shadow-stack-space
-                                                                   used-callee)
-                                                  acc))
-                                 ""
-                                 label-block*)])
-       (string-append label-block* prelude conclusion))]))
+(define (print-restore stack-space shadow-stack-space used-callee)
+  (string-append (print-gc-restore shadow-stack-space)
+                 (print-alignment-restore stack-space used-callee)
+                 (print-pops used-callee)
+                 "\tpopq %rbp\n"))
 
-(define (print-x86 p)
-  (match p
-    [(ProgramDefs info def*)
-     (foldr (lambda (def a)
-              (string-append a
-                             (print-x86-def def)))
-            ""
-            def*)]))
+(define (print-gc-restore shadow-stack-space)
+  (if (> shadow-stack-space 0)
+    (format "\tsubq $~a, %r15\n" shadow-stack-space)
+    ""))
+
+(define (print-alignment-restore stack-space used-callee)
+  (format "\taddq $~a, %rsp\n" (if (zero? (modulo (+ stack-space
+                                                     (* 8 (length used-callee)))
+                                                  16))
+                                 stack-space
+                                 (+ 8 stack-space))))
+
+(define (print-pops used-callee)
+  (let ([l (for/list ([r used-callee])
+             (format "\tpopq %~a\n" (Reg-name r)))])
+    (apply string-append l)))
