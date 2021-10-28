198b90501c1c28f599a20fa68070d33b37dc415d
iambrj <joshibharathiramana@gmail.com>
2021-10-28 18:54:31 +0530

    Add Lambda to reveal functions

--- reveal-functions.rkt
+++ reveal-functions.rkt
@@ -4,32 +4,46 @@
 
 (provide reveal-functions)
 
-; Transform function references from just f to (FunRef f)
+; Transform function references from just f to (FunRefArity f arity)
 (define (reveal-functions p)
   (match p
     [(ProgramDefs info def*)
-     (let ([funs (map Def-name def*)])
+     (let ([fun->arity (for/list ([def def*])
+                         (cons (Def-name def) (length (Def-param* def))))])
        (ProgramDefs info (map (lambda (d)
                                 (struct-copy Def d
-                                             [body ((rf-exp funs) (Def-body d))]))
+                                             [body ((rf-exp fun->arity) (Def-body d))]))
                               def*)))]))
 
-(define ((rf-exp funs) e)
+(define ((rf-exp fun->arity) e)
   (match e
     [(Void)         (Void)]
     [(Int i)        (Int i)]
     [(Bool b)       (Bool b)]
-    [(Var v)        (if (member v funs) (FunRef v) (Var v))]
-    [(HasType e t)  (HasType ((rf-exp funs) e) t)]
-    [(Let v e b)    (Let v ((rf-exp funs) e) (rf-exp (remove v funs) b))]
-    [(If c t e)     (If ((rf-exp funs) c) ((rf-exp funs) t) ((rf-exp funs) e))]
-    [(Prim op es) #:when (member op '(read - + not < vector vector-length))
-     (Prim op (map (rf-exp funs) es))]
+    [(Var v)        (if (member v (dict-keys fun->arity))
+                      (FunRefArity v (dict-ref fun->arity v))
+                      (Var v))]
+    [(HasType e t)  (HasType ((rf-exp fun->arity) e) t)]
+    [(Let v e b)    (Let v ((rf-exp fun->arity) e)
+                         ((rf-exp (dict-remove fun->arity v)) b))]
+    [(If c t e)     (If ((rf-exp fun->arity) c)
+                        ((rf-exp fun->arity) t)
+                        ((rf-exp fun->arity) e))]
+    [(Prim op es) #:when (member op '(read - + not < vector vector-length
+                                           procedure-arity))
+     (Prim op (map (rf-exp fun->arity) es))]
     [(Prim 'vector-ref `(,e ,(Int i)))
-     (Prim 'vector-ref `(,((rf-exp funs) e) ,(Int i)))]
+     (Prim 'vector-ref `(,((rf-exp fun->arity) e) ,(Int i)))]
     [(Prim 'vector-set! `(,e1 ,(Int i) ,e2))
-     (Prim 'vector-set! `(,((rf-exp funs) e1) ,(Int i) ,((rf-exp funs) e2)))]
+     (Prim 'vector-set! `(,((rf-exp fun->arity) e1)
+                           ,(Int i) ,((rf-exp fun->arity) e2)))]
     ; Invariant : only top level function definitions are allowed, no clause
     ; needed for Def
     [(Apply fun arg*)
-     (Apply ((rf-exp funs) fun) (map (rf-exp funs) arg*))]))
+     (Apply ((rf-exp fun->arity) fun) (map (rf-exp fun->arity) arg*))]
+    [(Lambda arg* rtype body)
+     (let ([fun->arity (foldr (lambda (arg a)
+                                (dict-remove a arg))
+                              fun->arity
+                              arg*)])
+      (Lambda arg* rtype ((rf-exp fun->arity) body)))]))
